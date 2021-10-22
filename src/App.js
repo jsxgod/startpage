286f49582a3550d7c0a54c88b23ff58c3170592e
jsxgod <smykoo7@gmail.com>
2021-10-22 23:27:25 +0200

    Render settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, { useState } from "react";
+import "./sass/main.scss";
+import { Settings, SettingsButton } from "./components";
+
 function App() {
+  const [settingsOpened, setSettingsOpened] = useState(false);
   return (
     <div className="App">
+      <SettingsButton openSettings={setSettingsOpened} />
+      <Settings opened={settingsOpened} openSettings={setSettingsOpened}/>
     </div>
   );
 }
