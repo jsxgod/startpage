81af634e761596f9413c3a4235505c81905920ba
jsxgod <smykoo7@gmail.com>
2021-10-22 23:27:02 +0200

    Create CloseButton component

---
+++ src/components/CloseButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  IoCloseSharp as CloseIcon,
+  IoCloseOutline as CloseIconOutline,
+} from "react-icons/io5";
+
+const CloseButton = ({ openSettings }) => {
+  return (
+    <button
+      className="settings-close-button"
+      onClick={() => openSettings(false)}
+    >
+      <CloseIcon className="close-icon" />
+    </button>
+  );
+};
+
+export default CloseButton;
