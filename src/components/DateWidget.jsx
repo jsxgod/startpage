63f7449578a48108788005b931f15014f5a1106e
jsxgod <smykoo7@gmail.com>
2021-10-24 23:21:44 +0200

    Create DateWidget Component

---
+++ src/components/DateWidget.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const DateWidget = () => {
+  const [date, setDate] = useState(new Date(Date.now()));
+
+  useEffect(() => {
+    const interval = setInterval(() => setDate(new Date(Date.now())), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  const getDayName = (dayNumber) => {};
+  return (
+    <div className="date-widget-container">
+      <div className="day-wrapper">
+        {date.toLocaleString("en-us", { weekday: "long" })}
+      </div>
+      <div className="date-wrapper">{date.toLocaleDateString()}</div>
+      <div className="time-wrapper">
+        {date.getHours()} : {date.getMinutes()} :{" "}
+        {date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds()}
+      </div>
+    </div>
+  );
+};
+
+export default DateWidget;
