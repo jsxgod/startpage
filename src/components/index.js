e2cdcbe7a9dedc7cc2181e2be35101db1fc9b65a
jsxgod <smykoo7@gmail.com>
2021-10-27 10:20:37 +0200

    Clean up imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
+export { default as NavBar } from "./NavBar";
 export { default as Settings } from "./Settings";
 export { default as SettingsButton } from "./SettingsButton";
 export { default as CloseButton } from "./CloseButton";
+export { default as DateWidget } from "./DateWidget";
+export { default as ToDoList } from "./ToDoList";
