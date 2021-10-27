bf3015bc0fbc6a379cac06910a520b3f5d722663
jsxgod <smykoo7@gmail.com>
2021-10-27 10:20:20 +0200

    Move settings to NavBar. Render ToDoList.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import "./sass/main.scss";
-import { Settings, SettingsButton } from "./components";
+import { NavBar, ToDoList } from "./components";
 
 function App() {
   const [settingsOpened, setSettingsOpened] = useState(false);
   return (
-    <div className="App">
-      <SettingsButton openSettings={setSettingsOpened} />
-      <Settings opened={settingsOpened} openSettings={setSettingsOpened}/>
+    <div className="start-page">
+      <NavBar settingsOpened={settingsOpened} setSettingsOpened={setSettingsOpened}/>
+      <ToDoList />
     </div>
   );
 }
