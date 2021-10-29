9f31ff71c275d9617955cca280f9574970b2df9d
jsxgod <smykoo7@gmail.com>
2021-10-29 23:19:54 +0200

    Add new groups. Add history groups.

--- src/data/todo-groups.js
+++ src/data/todo-groups.js
@@ -5,14 +5,23 @@ import {
   BsAlarmFill,
   BsFillExclamationCircleFill,
   BsBriefcaseFill,
-  BsBookFill
+  BsBookFill,
+  BsCodeSlash,
+  BsCheckCircleFill,
+  BsFillXCircleFill,
+  BsFillPaletteFill,
+  BsMusicNoteBeamed,
 } from "react-icons/bs";
 
 import {
   IoSchool,
+  IoHome,
 } from "react-icons/io5";
+import {
+  RiCarFill,
+} from "react-icons/ri";
 
-const availableGroups = [
+const bookmarks = [
   {
     name: "important",
     icon: <BsFillExclamationCircleFill />,
@@ -21,13 +30,13 @@ const availableGroups = [
   },
   { name: "heart", 
     icon: <BsHeartFill />, 
-    color: "#F8BBD0",
+    color: "#f48fb1",
     selected: false,
   },
   {
     name: "star",
     icon: <BsStarFill />,
-    color: "#ffea00",
+    color: "#ffee58",
     selected: false,
   },
   
@@ -41,22 +50,74 @@ const availableGroups = [
   {
     name: "work",
     icon: <BsBriefcaseFill />,
-    color: "#6D4C41",
+    color: "#455a64",
     selected: false,
   },
   
   {
     name: "school",
     icon: <IoSchool />,
-    color: "#CFD8DC",
+    color: "#b3e5fc",
     selected: false,
   },
   {
     name: "books",
     icon: <BsBookFill />,
-    color: "#E0E0E0",
+    color: "#D7CCC8",
     selected: false,
+  },
+  {
+    name: "programming",
+    icon: <BsCodeSlash />,
+    color: "#03A062",
+    selected: false,
+  },
+  {
+    name: "art",
+    icon: <BsFillPaletteFill />,
+    color: "#E65100",
+    selected: false,
+  },
+  {
+    name: "music",
+    icon: <BsMusicNoteBeamed />,
+    color: "#304FFE",
+    selected: false,
+  },
+  {
+    name: "car",
+    icon: <RiCarFill />,
+    color: "#757575",
+    selected: false,
+  },
+  {
+    name: "home",
+    icon: <IoHome />,
+    color: "#795548",
+    selected: false,
+  },
+];
+
+const history = [
+  {
+    name: "completed",
+    icon: <BsCheckCircleFill />,
+    color: "#00c853",
+    selected: false,
+
+  },
+  {
+    name: "removed",
+    icon: <BsFillXCircleFill />,
+    color: "#cf291d",
+    selected: false,
+
   }
 ];
+
+const availableGroups = {
+  bookmarks,
+  history
+}
 
 export default availableGroups;
