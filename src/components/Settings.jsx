3fea2edaa5d0dc6a613c62058c464f1606f741ea
jsxgod <smykoo7@gmail.com>
2021-11-01 23:54:53 +0100

    Add LinksList data editing from textarea. Validate JSON data before submission.

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,23 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CloseButton } from "./";
 import {
   BsArrowRightSquare as LeftArrow,
   BsArrowLeftSquare as RightArrow,
 } from "react-icons/bs";
+import { FaSave, FaFire, FaTrashAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Settings = ({ opened, openSettings }) => {
   const settings = ["Links", "Theme", "Searchbar"];
   const [selectedSetting, setSelectedSetting] = useState(0);
+  const [linksEditorData, setLinksEditorData] = useState(() => {
+    const localLinksData = localStorage.getItem("links");
+    return localLinksData
+      ? JSON.stringify(JSON.parse(localLinksData), null, 2)
+      : '[\n\t{\n\t"title":\t"TITLE",\n"links": [{}]}]';
+  });
 
   const handleSettingClick = (event) => {
     setSelectedSetting(event.target.getAttribute("data-index"));
+  };
+
+  const handleSaveSettings = () => {
+    try {
+      JSON.parse(linksEditorData);
+      localStorage.setItem("links", linksEditorData);
+      window.location.reload();
+    } catch (error) {
+      alert(error);
+    }
   };
 
   return (
     <>
       {opened && (
         <div className="settings">
-          <div className="window-wrapper">
+          <div className="settings-wrapper">
             <div className="settings-options-container">
               <div className="settings-options-wrapper">
                 <div className="settings-title">Settings</div>
@@ -38,7 +56,41 @@ const Settings = ({ opened, openSettings }) => {
                 <CloseButton openSettings={openSettings} />
               </div>
             </div>
-            <div className="settings-window"></div>
+            <div className="setting-area">
+              {settings[selectedSetting] === "Links" && (
+                <div className="links-editor-wrapper">
+                  <textarea
+                    spellCheck="false"
+                    className="links-editor"
+                    value={linksEditorData}
+                    onChange={(event) => setLinksEditorData(event.target.value)}
+                  ></textarea>
+                </div>
+              )}
+              <div className="buttons-area">
+                <div className="buttons-container">
+                  <button
+                    className="settings-button"
+                    onClick={() => handleSaveSettings()}
+                  >
+                    <p>Save Changes</p>
+                    <FaSave className="settings-button-icon" />
+                  </button>
+                  <motion.button
+                    className="settings-button"
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                  >
+                    <p>Discard Changes</p>
+                    <FaFire className="settings-button-icon" />
+                  </motion.button>
+                  <button className="settings-button">
+                    <p>Reset All Settings</p>
+                    <FaTrashAlt className="settings-button-icon" />
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       )}
