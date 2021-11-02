d55b3881937f73d67eb208f4f563273b1e13cd6c
jsxgod <smykoo7@gmail.com>
2021-11-02 15:05:18 +0100

    Wrap the input with a form element. Query the submitted input as a google search.

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -5,19 +5,24 @@ const Searchbar = () => {
   const [searchbarInput, setSearchbarInput] = useState("");
   return (
     <div className="searchbar">
-      <div className="searchbar-container">
+      <form
+        action="https://www.google.com/search"
+        method="get"
+        className="searchbar-container"
+      >
         <div className="searchbar-icon-wrapper">
           <FcGoogle className="searchbar-icon" />
         </div>
         <input
           autoFocus
           type="text"
+          name="q"
           value={searchbarInput}
           placeholder="Let's google!"
           onChange={(event) => setSearchbarInput(event.target.value)}
           className="searchbar-input"
         />
-      </div>
+      </form>
     </div>
   );
 };
