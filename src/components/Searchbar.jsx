38a4ec11c79168f437564311f44d1a0f8c5e2a5f
jsxgod <smykoo7@gmail.com>
2021-11-02 22:17:45 +0100

    Add additional searchbar options: Reddit and Stack Overflow buttons that add their name to the input query when selected. Style this functionality.

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { BsReddit, BsStackOverflow } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 const Searchbar = () => {
   const [searchbarInput, setSearchbarInput] = useState("");
+  const [searchOption, setSearchOption] = useState(() => {
+    const localSearchOption = localStorage.getItem("searchOption");
+    return localSearchOption ? localSearchOption : "";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("searchOption", searchOption);
+  }, [searchOption]);
 
   const checkForwardSearch = () => {
     const fs = JSON.parse(localStorage.getItem("forward-search")).filter(
@@ -12,14 +22,45 @@ const Searchbar = () => {
       console.log(fs[0].destination);
       window.location.href = "https://" + fs[0].destination;
     } else {
-      window.location.href = "https://google.com/search?q=" + searchbarInput;
+      window.location.href =
+        "https://google.com/search?q=" + searchbarInput + " " + searchOption;
+    }
+  };
+
+  const toggleSearchOption = (option) => {
+    if (searchOption === "") {
+      setSearchOption(option);
+    } else if (searchOption === option) {
+      setSearchOption("");
+    } else {
+      setSearchOption(option);
     }
   };
 
   return (
     <div className="searchbar">
       <div className="searchbar-container">
-        <div className="searchbar-icon-wrapper">
+        <motion.button
+          className={`searchbar-option-icon-button ${
+            searchOption === "reddit" ? "reddit" : ""
+          }`}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.8 }}
+          onClick={() => toggleSearchOption("reddit")}
+        >
+          <BsReddit />
+        </motion.button>
+        <motion.button
+          className={`searchbar-option-icon-button ${
+            searchOption === "stack overflow" ? "stackOverflow" : ""
+          }`}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.8 }}
+          onClick={() => toggleSearchOption("stack overflow")}
+        >
+          <BsStackOverflow />
+        </motion.button>
+        <div className="searchbar-engine-icon-wrapper">
           <FcGoogle className="searchbar-icon" />
         </div>
         <input
