7a2aa42f8c2f2d1ac7360d4b9826e74d1dd38e55
jsxgod <smykoo7@gmail.com>
2021-11-02 19:53:10 +0100

    Remove form element. Add forward search lookup.

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,26 +3,39 @@ import { FcGoogle } from "react-icons/fc";
 
 const Searchbar = () => {
   const [searchbarInput, setSearchbarInput] = useState("");
+
+  const checkForwardSearch = () => {
+    const fs = JSON.parse(localStorage.getItem("forward-search")).filter(
+      (f) => f.search === searchbarInput
+    );
+    if (fs[0]) {
+      console.log(fs[0].destination);
+      window.location.href = "https://" + fs[0].destination;
+    } else {
+      window.location.href = "https://google.com/search?q=" + searchbarInput;
+    }
+  };
+
   return (
     <div className="searchbar">
-      <form
-        action="https://www.google.com/search"
-        method="get"
-        className="searchbar-container"
-      >
+      <div className="searchbar-container">
         <div className="searchbar-icon-wrapper">
           <FcGoogle className="searchbar-icon" />
         </div>
         <input
           autoFocus
           type="text"
-          name="q"
+          onKeyUp={(event) => {
+            if (event.key === "Enter") {
+              checkForwardSearch();
+            }
+          }}
           value={searchbarInput}
           placeholder="Let's google!"
           onChange={(event) => setSearchbarInput(event.target.value)}
           className="searchbar-input"
         />
-      </form>
+      </div>
     </div>
   );
 };
