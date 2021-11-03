c349a431c86885aeb784334c6e51e075f90c6a52
jsxgod <smykoo7@gmail.com>
2021-11-03 14:37:01 +0100

    Add new icons. Add new search options. Add the ability to toggle engines. Use the engine object to determine the query and rework the search handler. Handle Google Scholar search (no search options). Use localStorage to make the settings 'sticky'. Disable search option buttons and hover animations while Google Scholar engine is selected.

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
-import { BsReddit, BsStackOverflow } from "react-icons/bs";
+import {
+  BsReddit,
+  BsStackOverflow,
+  BsFillFileEarmarkPdfFill,
+} from "react-icons/bs";
+import { SiGooglescholar, SiDuckduckgo } from "react-icons/si";
 import { motion } from "framer-motion";
 
 const Searchbar = () => {
@@ -9,21 +14,52 @@ const Searchbar = () => {
     const localSearchOption = localStorage.getItem("searchOption");
     return localSearchOption ? localSearchOption : "";
   });
+  const [engine, setEngine] = useState(() => {
+    const localEngine = localStorage.getItem("engine");
+    return localEngine
+      ? JSON.parse(localEngine)
+      : {
+          name: "google",
+          queryURL: "https://google.com/search?q=",
+        };
+  });
+  const [hoverEnabled, setHoverEnabled] = useState(
+    engine.name !== "googleScholar"
+  );
 
   useEffect(() => {
     localStorage.setItem("searchOption", searchOption);
   }, [searchOption]);
 
-  const checkForwardSearch = () => {
+  useEffect(() => {
+    localStorage.setItem("engine", JSON.stringify(engine));
+  }, [engine]);
+
+  const handleSearch = () => {
     const fs = JSON.parse(localStorage.getItem("forward-search")).filter(
       (f) => f.search === searchbarInput
-    );
-    if (fs[0]) {
-      console.log(fs[0].destination);
-      window.location.href = "https://" + fs[0].destination;
+    )[0];
+
+    if (fs) {
+      console.log(fs.destination);
+      window.location.href = "https://" + fs.destination;
     } else {
-      window.location.href =
-        "https://google.com/search?q=" + searchbarInput + " " + searchOption;
+      if (engine.name === "googleScholar") {
+        window.location.href = engine.queryURL + searchbarInput;
+      } else if (engine.name === "google" || engine.name === "duckDuckGo") {
+        if (searchOption === "reddit") {
+          window.location.href =
+            engine.queryURL + "site:reddit.com " + searchbarInput;
+        } else if (searchOption === "stack overflow") {
+          window.location.href =
+            engine.queryURL + "site:stackoverflow.com " + searchbarInput;
+        } else if (searchOption === "pdf") {
+          window.location.href =
+            engine.queryURL + "filetype:pdf " + searchbarInput;
+        } else {
+          window.location.href = engine.queryURL + searchbarInput;
+        }
+      }
     }
   };
 
@@ -37,14 +73,39 @@ const Searchbar = () => {
     }
   };
 
+  const toggleEngine = (engine) => {
+    if (engine === "google") {
+      setEngine({
+        name: "duckDuckGo",
+        queryURL: "https://duckduckgo.com/?q=",
+      });
+      setHoverEnabled(true);
+    } else if (engine === "duckDuckGo") {
+      setEngine({
+        name: "googleScholar",
+        queryURL: "https://scholar.google.com/scholar?q=",
+      });
+      setHoverEnabled(false);
+    } else {
+      setEngine({
+        name: "google",
+        queryURL: "https://google.com/search?q=",
+      });
+      setHoverEnabled(true);
+    }
+  };
+
   return (
     <div className="searchbar">
       <div className="searchbar-container">
         <motion.button
           className={`searchbar-option-icon-button ${
-            searchOption === "reddit" ? "reddit" : ""
+            searchOption === "reddit" && engine.name !== "googleScholar"
+              ? "reddit"
+              : ""
           }`}
-          whileHover={{ scale: 1.1 }}
+          disabled={engine.name === "googleScholar"}
+          whileHover={hoverEnabled ? { scale: 1.1 } : { scale: 1 }}
           whileTap={{ scale: 0.8 }}
           onClick={() => toggleSearchOption("reddit")}
         >
@@ -52,23 +113,50 @@ const Searchbar = () => {
         </motion.button>
         <motion.button
           className={`searchbar-option-icon-button ${
-            searchOption === "stack overflow" ? "stackOverflow" : ""
+            searchOption === "stack overflow" && engine.name !== "googleScholar"
+              ? "stackOverflow"
+              : ""
           }`}
-          whileHover={{ scale: 1.1 }}
+          disabled={engine.name === "googleScholar"}
+          whileHover={hoverEnabled ? { scale: 1.1 } : { scale: 1 }}
           whileTap={{ scale: 0.8 }}
           onClick={() => toggleSearchOption("stack overflow")}
         >
           <BsStackOverflow />
         </motion.button>
-        <div className="searchbar-engine-icon-wrapper">
-          <FcGoogle className="searchbar-icon" />
-        </div>
+        <motion.button
+          className={`searchbar-option-icon-button ${
+            searchOption === "pdf" && engine.name !== "googleScholar"
+              ? "pdf"
+              : ""
+          }`}
+          disabled={engine.name === "googleScholar"}
+          whileHover={hoverEnabled ? { scale: 1.1 } : { scale: 1 }}
+          whileTap={{ scale: 0.8 }}
+          onClick={() => toggleSearchOption("pdf")}
+        >
+          <BsFillFileEarmarkPdfFill />
+        </motion.button>
+        <motion.button
+          className={`searchbar-engine-icon-button ${engine.name}`}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.8 }}
+          onClick={() => toggleEngine(engine.name)}
+        >
+          {engine.name === "google" ? (
+            <FcGoogle />
+          ) : engine.name === "googleScholar" ? (
+            <SiGooglescholar />
+          ) : (
+            <SiDuckduckgo />
+          )}
+        </motion.button>
         <input
           autoFocus
           type="text"
           onKeyUp={(event) => {
             if (event.key === "Enter") {
-              checkForwardSearch();
+              handleSearch();
             }
           }}
           value={searchbarInput}
