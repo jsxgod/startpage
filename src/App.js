129d161c94f2171a4d7bc36e22042d955e3f058b
jsxgod <smykoo7@gmail.com>
2021-11-04 23:40:29 +0100

    Load theme on initial page load.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./sass/main.scss";
 import { LinksList, NavBar, Searchbar, ToDoList } from "./components";
 
 function App() {
   const [settingsOpened, setSettingsOpened] = useState(false);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    theme 
+      ? document.documentElement.setAttribute("data-theme", theme)
+      : document.documentElement.setAttribute("data-theme", "dark");
+  }, [])
   return (
       <div className="start-page">
         <NavBar settingsOpened={settingsOpened} setSettingsOpened={setSettingsOpened}/>
