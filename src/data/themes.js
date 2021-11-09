1d8010c281a10e6cb43b442255cb78e5037401cf
jsxgod <smykoo7@gmail.com>
2021-11-09 20:00:11 +0100

    Store themes in a file

---
+++ src/data/themes.js
@@ -0,0 +1,52 @@
+const themes = [
+  {
+    name: "dark",
+    preview: process.env.PUBLIC_URL + "./assets/images/themes/dark_theme.png",
+    alt: "dark_theme.png",
+    mono: "false"
+  },
+  {
+    name: "light",
+    preview: process.env.PUBLIC_URL + "./assets/images/themes/light_theme.png",
+    alt: "light_theme.png",
+    mono: "false"
+  },
+  {
+    name: "nihon",
+    preview: process.env.PUBLIC_URL + "./assets/images/themes/nihon_theme.png",
+    alt: "nihon_theme.png",
+    mono: "false"
+  },
+  {
+    name: "japanese-tree",
+    preview: process.env.PUBLIC_URL + "./assets/images/themes/japanese_tree_theme.png",
+    alt: "japanese_tree_theme.png",
+    mono: "false"
+  },
+  {
+    name: "koi-pond",
+    preview: process.env.PUBLIC_URL + "./assets/images/themes/koi_pond_theme.png",
+    alt: "koi_pond_theme.png",
+    mono: "false"
+  },
+  {
+    name: "purple-haze",
+    preview: process.env.PUBLIC_URL + "./assets/images/themes/purple_haze_theme.png",
+    alt: "purple_haze_theme.png",
+    mono: "false"
+  },
+  {
+    name: "mono-dark",
+    preview: process.env.PUBLIC_URL + "./assets/images/themes/mono_dark_theme.png",
+    alt: "mono_dark_theme.png",
+    mono: "true"
+  },
+  {
+    name: "mono-light",
+    preview: process.env.PUBLIC_URL + "./assets/images/themes/mono_light_theme.png",
+    alt: "mono_light_theme.png",
+    mono: "true"
+  },
+]
+
+export default themes;
